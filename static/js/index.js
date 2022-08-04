872b3c383d09d5a7e61a946f3a3f2d3e9c076bfb
airwarriorg91 <gauravxpgupta@gmail.com>
2022-08-05 00:32:05 +0530

    edit

--- static/js/index.js
+++ static/js/index.js
@@ -2,7 +2,5 @@ var preloader = document.querySelector("#preloader");
 window.addEventListener("load", function() {
     //hide the preloader
     preloader.style.display = "none";
-    let node = document.querySelector('.preload-transitions');
-    node.classList.remove('preload-transitions');
 });
 
