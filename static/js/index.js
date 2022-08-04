1db14070606ee178d431e44add5a34923bb99c08
airwarriorg91 <gauravxpgupta@gmail.com>
2022-08-05 00:26:32 +0530

    edit

--- static/js/index.js
+++ static/js/index.js
@@ -2,5 +2,7 @@ var preloader = document.querySelector("#preloader");
 window.addEventListener("load", function() {
     //hide the preloader
     preloader.style.display = "none";
+    let node = document.querySelector('.preload-transitions');
+    node.classList.remove('preload-transitions');
 });
 
