c81ddcf864958ca9f8d15baf1c4d106a1f39a9d8
airwarriorg91 <gauravxpgupta@gmail.com>
2022-09-23 01:10:43 +0530

    'edit1'

--- comingsoon/settings.py
+++ comingsoon/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import dj_database_url
 import os
+import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
