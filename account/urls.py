1ea96a54ae33989921892755caf7b68c04184c70
airwarriorg91 <gauravxpgupta@gmail.com>
2022-09-29 09:59:15 +0530

    'debugging1'

--- account/urls.py
+++ account/urls.py
@@ -2,8 +2,8 @@ from . import views
 from django.urls import path, include
 
 urlpatterns = [
-    path('save', views.saveAccount, name='save'),
-    path('login', views.loginView, name='login'),
+    path('save/', views.saveAccount, name='save'),
+    path('login/', views.loginView, name='login'),
     # path('authenticate', views.authenticate_defined, name='authenticate_defined'),
     path('activate/<uidb64>/<token>',views.VerificationView.as_view(), name='activate'),
     path('continue-registration', views.continueView, name='continue'),
